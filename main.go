ccb651db6d60fca76954114c40e67839d518ad09
Simon Pratt <simon@pr4tt.com>
2017-02-02 08:49:56 -0500

    static html, building asm

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-  "log"
+  "io/ioutil"
+  "bytes"
   "fmt"
+  "log"
   "net/http"
   "os"
+  "os/exec"
 )
 
 func determineListenAddress() (string, error) {
@@ -15,8 +18,40 @@ func determineListenAddress() (string, error) {
   return ":" + port, nil
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello World")
+func getAsm(w http.ResponseWriter, r *http.Request) {
+  // write asm to temp file
+  content := []byte(`package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Hello, world")
+}
+`)
+  tmpfile, err := ioutil.TempFile("", "goasm-")
+  if err != nil {
+    fmt.Fprintln(w, "Error writing to file")
+  }
+  defer os.Remove(tmpfile.Name()) // clean up
+  if _, err := tmpfile.Write(content); err != nil {
+    log.Fatal(err)
+  }
+  if err := tmpfile.Close(); err != nil {
+    log.Fatal(err)
+  }
+
+  // compile
+  cmd := exec.Command("go", "tool", "compile", "-o", "/dev/null", "-S",
+                      tmpfile.Name())
+  var out bytes.Buffer
+  cmd.Stdout = &out
+  err = cmd.Run()
+  if err != nil {
+    fmt.Fprintln(w, "Error compiling")
+  }
+  fmt.Fprintln(w, out.String())
 }
 
 func main() {
@@ -25,7 +60,8 @@ func main() {
     log.Fatal(err)
   }
 
-  http.HandleFunc("/", hello)
+  http.Handle("/", http.FileServer(http.Dir("./static")))
+  http.HandleFunc("/asm", getAsm)
   log.Printf("Listening on %s...\n", addr)
   if err := http.ListenAndServe(addr, nil); err != nil {
     panic(err)
